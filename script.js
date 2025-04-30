6e164bac88637735f00f6e7df75f4a201d9e3863
Saurabh <142657062+saurabh19304@users.noreply.github.com>
2025-05-01 03:03:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const passwordBox =  document.getElementById('password');
+const length = 12;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const number = '0123456789';
+const symbol = '!@#$%^&*()[]{}`+-=<>/\|~.,:;?_';
+
+const allChar = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+  let password ='';
+
+  password += upperCase[Math.floor(Math.random()*upperCase.length)];
+  password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+  password += number[Math.floor(Math.random()*number.length)];
+  password += symbol[Math.floor(Math.random()*symbol.length)];
+
+  while(length > password.length){
+    password += allChar[Math.floor(Math.random()*allChar.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword(){
+  navigator.clipboard.writeText(passwordBox.value)
+  .then(() => alert("Password copied to clipboard!"))
+  .catch(err => alert("Failed to copy: " + err));
+
+}
